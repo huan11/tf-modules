bc60f3af720d05f046f66932a9be6e451be0c85f
tfbj <sherrillmy@hotmail.com>
2022-03-17 11:49:25 +0800

    Update compute.tf

--- tf_aws/compute.tf
+++ tf_aws/compute.tf
@@ -2,6 +2,6 @@ resource "aws_instance" "ec2_example" {
     ami = "${lookup(var.webserver_amis, var.aws_region)}" 
     instance_type = "t2.micro" 
     tags = {
-        Name = "Terraform EC2"
+        Name = "Terraform EC2 AWS"
     }
 }
