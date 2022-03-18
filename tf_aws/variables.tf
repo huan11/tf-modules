607f6f6f53c639930d00caa54ee12b8ad72e2385
tfbj <sherrillmy@hotmail.com>
2022-03-18 09:14:35 +0800

    Update variables.tf

--- tf_aws/variables.tf
+++ tf_aws/variables.tf
@@ -12,12 +12,3 @@ variable "webserver_amis" {
 
   type = map
 }
-variable "aws_access_key" {
-  type        = string
-  default     = "AKIAT6C627FS3FW7YJP4"
-}
-
-variable "aws_secret_key" {
-  type        = string
-  default     = "kssAyYq0XDP/Ccr7mhSIoZ3wzI5r/Ntuiz+4NSXn"
-}
